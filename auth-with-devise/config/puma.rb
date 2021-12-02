3cbfd4ac30f52590c6aab5a8ca1e7ec933b22163
katsuya yamaguchi <ka.yamaguchi.3@gmail.com>
2021-12-02 16:36:01 +0900

    #1 Railsアプリが起動するように設定変更。
    - Pumaの設定変更。
    - docker-composeでWEB-APP間が接続できるように設定変更。

--- auth-with-devise/config/puma.rb
+++ auth-with-devise/config/puma.rb
@@ -1,3 +1,4 @@
+app_path = File.expand_path('..', __dir__)
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -15,7 +16,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -30,14 +31,20 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
+
+bind "unix://#{app_path}/tmp/sockets/puma.sock"
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
